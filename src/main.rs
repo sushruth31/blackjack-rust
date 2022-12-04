7bd37bfa974dbabe896a7c1d73925a37d0ceb86c
sushruth31 <sushruth31@gmail.com>
2022-12-04 00:47:23 -0500

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
 mod utils;
 use rand::Rng;
 use std::{
+    borrow::{Borrow, BorrowMut},
     collections::HashMap,
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    pin::Pin,
     sync::Arc,
 };
 use utils::*;
 
-use tokio::{
-    io::AsyncWriteExt,
-    net::TcpStream,
-    sync::{mpsc, Mutex},
-};
+use tokio::sync::{mpsc, Mutex};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Player {
     name: String,
     money: i32,
@@ -35,12 +33,12 @@ impl Player {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Dealer {
     cards: Vec<Card>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Game {
     player_pool: Vec<Player>,
     deck: Deck,
@@ -59,47 +57,120 @@ impl Game {
     }
 }
 
+type PlayerChannels = Arc<
+    Mutex<
+        HashMap<
+            String,
+            (
+                mpsc::Sender<String>,
+                Arc<Mutex<mpsc::Receiver<String>>>,
+                TcpStream,
+            ),
+        >,
+    >,
+>;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("localhost:8080")?;
     let player_lobby: Arc<Mutex<Vec<Player>>> = Arc::new(Mutex::new(Vec::new()));
-    let player_channels: Arc<Mutex<HashMap<String, tokio::sync::mpsc::Sender<String>>>> =
-        Arc::new(Mutex::new(HashMap::new()));
     let player_lobby_clone = player_lobby.clone();
+    let player_channels: PlayerChannels = Arc::new(Mutex::new(HashMap::new()));
     let player_channels_clone = player_channels.clone();
+    let player_channels_clone2 = player_channels.clone();
     let mut deck = Deck::new();
     deck.shuffle();
-    let mut game = Game {
+    let game = Arc::new(Mutex::new(Game {
         player_pool: Vec::new(),
         deck,
         dealer: Dealer { cards: Vec::new() },
+    }));
+
+    //function that will send to all players in the game
+    let broadcast = |msg: String, from: String| async move {
+        let player_channels = Arc::clone(&player_channels_clone2);
+        let player_channels = player_channels.lock().await;
+        player_channels.iter().for_each(|(id, (tx, rx, stream))| {
+            if id != &from {
+                let res = tx.try_send(msg.clone());
+                if res.is_err() {
+                    println!("Error sending to player {}", id);
+                } else {
+                    println!("Sent to player {}", id);
+                }
+            }
+        });
     };
 
     tokio::spawn(async move {
+        let game = Arc::clone(&game);
         //move players from lobby to game
         loop {
+            let mut game = game.lock().await;
             //if no players in lobby wait
             if player_lobby.lock().await.len() == 0 {
                 continue;
             }
-            if game.deck.0.len() < 10 {
+            if game.borrow().deck.0.len() < 10 {
                 game.deck = Deck::new();
                 game.deck.shuffle();
             }
+            //give the dealer a card
+            if let Some(card) = game.deck.deal_card() {
+                game.dealer.cards.push(card);
+            } else {
+                continue;
+            }
             let mut players = player_lobby.lock().await;
             for player in players.drain(..) {
+                let mut game = game.clone();
+                let broadcast = broadcast.clone();
                 game.add_player(&player.name, &player.id);
                 println!("{} {}, joined the game", player.name, player.id);
                 //get the channel for the player to send messages to
                 let mut channels = player_channels.lock().await;
-                let channel = channels.get_mut(&player.id).unwrap();
+                let (channel, rx, stream) = channels.get_mut(&player.id).unwrap();
                 //send the player a message that they have joined the game
                 channel
-                    .send("You have joined the game".to_string())
+                    .send(format!("You have joined the game, {}!", player.name))
                     .await
                     .unwrap();
+                //need to drop the lock on the channels before broadcasting
+                drop(channels);
+                drop(game);
+                //send all other players a message that a new player has joined
+                broadcast(
+                    format!("Testing the brocast. {} joined the game", player.name,),
+                    player.id.clone(),
+                )
+                .await;
+                //loop through all the players and deal them cards
+                let channels = player_channels.lock().await;
+                let (tx, rx, stream) = channels.get(&player.id).unwrap();
+                let mut rx = rx.lock().await;
+                tx.send(format!("Ok, {}, it's your turn. Place a bet", player.name))
+                    .await
+                    .unwrap();
+                //await the player to send a message back that is a number
+                let mut buf = [0; 1024];
+                let mut stream = stream.try_clone().unwrap();
+                let mut bet = 0;
+                loop {
+                    let n = stream.read(&mut buf).unwrap();
+                    let msg = String::from_utf8_lossy(&buf[..n]);
+                    let msg = msg.trim();
+                    if let Ok(_bet) = msg.parse::<i32>() {
+                        if _bet > 0 {
+                            tx.send(format!("You bet {}", _bet)).await.unwrap();
+                            bet = _bet;
+                            break;
+                        }
+                    }
+                    tx.send("Please enter a valid bet".to_owned())
+                        .await
+                        .unwrap();
+                }
             }
-            //loop through all the players and deal them cards
         }
     });
 
@@ -120,16 +191,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut lobby = lobby.lock().await;
             let mut player_channels = player_channels_clone.lock().await;
             //create a channel for the player and add it to the hashmap
-            let (tx, mut rx) = mpsc::channel(32);
-            player_channels.insert(id.clone(), tx);
+            let (tx, rx) = mpsc::channel(32);
+            let rx = Arc::new(Mutex::new(rx));
+            let rx_copy = rx.clone();
+            let stream_copy = stream.try_clone().unwrap();
+            player_channels.insert(id.clone(), (tx, rx_copy, stream_copy));
             lobby.push(Player::new(&name, &id));
-            //print players in lobby
-            for player in lobby.iter() {
-                println!("{} is in the lobby", player.name);
-            }
-            std::mem::drop(lobby);
-            std::mem::drop(player_channels);
+            drop(lobby);
+            drop(player_channels);
             loop {
+                let mut rx = rx.lock().await;
                 //read messages from channel and send them to the player
                 if let Some(message) = rx.recv().await {
                     stream.write(message.as_bytes()).unwrap();
@@ -141,7 +212,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Deck(Vec<Card>);
 
 impl Deck {
@@ -168,7 +239,7 @@ impl Deck {
         }
     }
 
-    fn deal_card(&mut self) -> Card {
-        self.0.pop().unwrap()
+    fn deal_card(&mut self) -> Option<Card> {
+        return self.0.pop();
     }
 }
